09f3d48751c5519cb3be4b645de0820a12aceb03
Fedora <connor178180@gmail.com>
2023-10-01 22:41:26 +1100

    g
    g

--- Full_Scanner/New code/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-    ret, frame = self.vid.read()
-        if ret:
-            adjusted_frame = self.adjust_brightness_and_contrast(frame)
-            self.photo = ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(adjusted_frame, cv2.COLOR_BGR2RGB)))
-            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-        self.window.after(10, self.update)
